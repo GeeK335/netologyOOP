b459e809b2ced639a45d018282ff7ce1db510ecd
Sergey Tormozov <web@stormozov.ru>
2024-05-31 17:29:12 +0300

    I have added documentation for the methods and functions and have resolved four issues.
    Signed-off-by: Sergey Tormozov <web@stormozov.ru>

--- task_4.py
+++ task_4.py
@@ -1,5 +1,13 @@
 # Helper classes
 class PersonalInfo:
+	"""
+	A class to represent personal information of an individual.
+
+	Attributes:
+		name (str): The first name of the individual.
+		surname (str): The last name of the individual.
+		gender (str): The gender of the individual.
+	"""
 	def __init__(self, name: str, surname: str, gender: str):
 		self.name = name
 		self.surname = surname
@@ -8,11 +16,17 @@ class PersonalInfo:
 
 class MathMethods:
 	def average(self, grades: list):
-		# Check if the lecturer has grades
-		if len(list(grades)):
-			# I combine student grades for different courses
-			merged_list = [el for lists in list(grades) for el in lists]
-			# Finding the average and rounding the value to one decimal place
+		"""
+		Calculate the average grade from a list of grades.
+
+		Args:
+			grades (list): A list of grades for a student.
+
+		Returns:
+			float: The average grade rounded to one decimal place.
+		"""
+		if grades:
+			merged_list = [el for lists in grades for el in lists]
 			return round(sum(merged_list) / len(merged_list), 1)
 		else:
 			return 'Еще нет оценок'
@@ -34,7 +48,7 @@ class CompareTwoPeople:
 			return f'{lector_info[1]} {text} {lector_info[3]}'
 		else:
 			text = (f'{lector_info[0]} и {lector_info[1]} одинаково результативны. Их балл '
-			        f'составляет: {lector_info[3]}')
+					f'составляет: {lector_info[3]}')
 			return f'Лекторы {text}'
 
 
@@ -90,7 +104,7 @@ class Lecturer(Mentor, MathMethods, CompareTwoPeople):
 	def __str__(self) -> str:
 		self.average_value = self.average(list(self.grades.values()))
 		return (f'Имя: {self.name}\nФамилия: {self.surname}\n'
-		        f'Средняя оценка за лекции: {self.average_value}')
+				f'Средняя оценка за лекции: {self.average_value}')
 
 
 # Reviewers class
@@ -142,40 +156,84 @@ garik_reviewer.rate_student(sergey_makarov, 'Git', [6, 8])
 maxim_reviewer.rate_student(sergey_makarov, 'Python', [9, 10, 7])
 
 # A student gives a grade to the lecture for a course
-sergey_makarov.rate_lecturer(oleg_lecturer, 'Git', 5)
+evelina_sokolova.rate_lecturer(oleg_lecturer, 'Git', 8)
 evelina_sokolova.rate_lecturer(oleg_lecturer, 'Python', 10)
 evelina_sokolova.rate_lecturer(dima_lecturer, 'Python', 9)
+sergey_makarov.rate_lecturer(oleg_lecturer, 'Git', 5)
 sergey_makarov.rate_lecturer(oleg_lecturer, 'Python', 7)
 sergey_makarov.rate_lecturer(dima_lecturer, 'Python', 8)
 
+print('= TASK 1 and 2 =')
 # Displaying the result of the code on the screen for checking
 print('[Student] Evelina Sokolova:', evelina_sokolova.grades)
 print('[Student] Sergey Makarov:', sergey_makarov.grades)
 print('[Lecturer] Oleg Temnov:', oleg_lecturer.grades)
 
 # Print for 3 Task
-print('', '— Reviewer —', maxim_reviewer, sep='\n')
+print('', '= TASK 3 =', '#1 Перезагрузка метода', sep='\n')
+print('— Reviewer —', maxim_reviewer, sep='\n')
 print('', '— 1 Lecturer —', oleg_lecturer, sep='\n')
 print('', '— 2 Lecturer —', dima_lecturer, sep='\n')
 print('', '— 1 Student —', evelina_sokolova, sep='\n')
 print('', '— 2 Student —', sergey_makarov, sep='\n')
 
-print('', 'Задача: Сравнение лекторов и студентов', sep='\n')
+print('', '#2 Реализуйте возможность сравнивать', sep='\n')
 print(oleg_lecturer.compare(dima_lecturer))
 print(evelina_sokolova.compare(sergey_makarov))
-print()
+
+
+# Counting duplicate keys in different dictionaries
+def count_unique_keys(dicts):
+	"""
+	A function that takes a list of dictionaries and returns a dictionary where the keys are
+	unique keys from all dictionaries in the input list and the values are the number of times
+	each key appears across all dictionaries.
+	"""
+
+	keys_number = {}
+	for dict_ in dicts:
+		for key in dict_:
+			keys_number[key] = keys_number.get(key, 0) + 1
+	return keys_number
 
 
 # Calculating the average grade for homework for all students in a particular course
-def average_all_students(course_name: str, *students):
+def average_rating(course_name: str, *role):
+	"""
+	Calculate the average rating for a specific course from multiple dictionaries.
+
+	Parameters:
+	- course_name (str): The name of the course for which the average rating is calculated.
+	- *role (dict): Variable number of dictionaries containing course ratings.
+
+	Returns:
+	- float: The average rating for the specified course across all dictionaries.
+
+	Example:
+	average_rating('Math', {'Math': [4, 5, 3]}, {'Math': [2, 4, 3]})
+	"""
+	# The number of keys in the dictionary
+	keys = count_unique_keys(role)
 	combined_grades = []
-	for i in range(len(students)):
-		combined_grades += students[i][course_name]
-	average_rating = sum(combined_grades) / len(combined_grades)
-	return average_rating
+	#
+	if keys[course_name] > 1:
+		# If there are more than one identical key, we will add the values
+		# associated with those keys to a common list.
+		for i in range(len(role)):
+			combined_grades += role[i][course_name]
+	else:
+		# If the desired key is found in only one dictionary,
+		# we will get its corresponding value
+		for dict_ in role:
+			if course_name in dict_:
+				combined_grades += dict_[course_name]
+	return round(sum(combined_grades) / len(combined_grades), 1)
 
 
-average_student = ('Средняя оценка за домашние задания по всем студентам в рамках '
-                   'конкретного курса:')
-print(average_student, average_all_students('Git', evelina_sokolova.grades,
-                                            sergey_makarov.grades))
+average_s_text = 'Средняя оценка за домашние задания по всем студентам в рамках конкретного курса:'
+average_l_text = 'Средняя оценка за лекции всех лекторов в рамках конкретного курса:'
+
+print('', '= TASK 4 =', sep='\n')
+print(average_s_text,
+	  average_rating('Python', evelina_sokolova.grades, sergey_makarov.grades))
+print(average_l_text, average_rating('Git', oleg_lecturer.grades, dima_lecturer.grades))
